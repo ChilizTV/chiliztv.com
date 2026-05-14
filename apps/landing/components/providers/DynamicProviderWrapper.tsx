dc47fa4558267bea44cd56870362937287a32eaf
Antonybyrt <antony.loussararian@gmail.com>
2026-05-14 15:05:08 +0200

    ficx(devops): lint and tests

--- apps/landing/components/providers/DynamicProviderWrapper.tsx
+++ apps/landing/components/providers/DynamicProviderWrapper.tsx
@@ -74,10 +74,6 @@ export default function DynamicProviderWrapper({
     events: {},
   };
 
-  if (!environmentId) {
-    return <>{children}</>;
-  }
-
   return (
     <WagmiProviderWrapper>
       <DynamicContextProvider settings={dynamicSettings}>
